4d0bb49a7d8d0d12c9f5c5d1e33e59aa80eed0bb
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2023-11-19 03:48:50 +0530

    quality of life improvements

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,12 +1,9 @@
+"use client"
+import { Redis } from "@upstash/redis";
 
-import {Redis} from '@upstash/redis'
-// import {Redis as Iredis} from 'ioredis'
-// const redis = new Redis()
 const redis = new Redis({
-    url:process.env.UPSTASH_REDIS_REST_URL as string,
-    token:process.env.UPSTASH_REDIS_REST_TOKEN as string
-})
+  url: process.env.UPSTASH_REDIS_REST_URL as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+});
 
-
-
-export default redis
+export default redis;
