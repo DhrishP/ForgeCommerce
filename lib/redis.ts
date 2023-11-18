01b36caf833727d85aa612912545abeea88a3dc3
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2023-11-19 02:28:26 +0530

    cloud cache data

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,12 +1,12 @@
 
 import {Redis} from '@upstash/redis'
-import {Redis as Iredis} from 'ioredis'
+// import {Redis as Iredis} from 'ioredis'
+// const redis = new Redis()
+const redis = new Redis({
+    url:process.env.UPSTASH_REDIS_REST_URL as string,
+    token:process.env.UPSTASH_REDIS_REST_TOKEN as string
+})
 
-// const redis = new Redis({
-//     url:process.env.UPSTASH_REDIS_REST_URL as string,
-//     token:process.env.UPSTASH_REDIS_REST_TOKEN as string
-// })
 
-const redis = new Iredis()
 
 export default redis
