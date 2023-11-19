237942a4b599f9659bafd63d7d6749b8467c4fad
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2023-11-20 03:48:42 +0530

    added ratelimting

--- lib/rate-limit.ts
+++ lib/rate-limit.ts
@@ -3,7 +3,7 @@ import redis from "@/lib/redis";
 
 const ratelimit = new Ratelimit({
     redis: redis,
-    limiter: Ratelimit.slidingWindow(2, "10 s"),
+    limiter: Ratelimit.slidingWindow(30, "30 s"),
   });
 
  export default ratelimit; 
