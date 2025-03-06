72b3fede0a38765b032e384e082d2726fe0db6e0
DhrishP <parekhdhrish.pg@gmail.com>
2025-03-06 10:55:19 +0530

    update : middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"]
+  publicRoutes: ["/api/:path*", "/clkwya8dw0000lb082nevtsqf"],
 });
 
 export const config = {
